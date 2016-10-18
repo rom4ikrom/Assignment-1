c9b956d83e50fd1010040535fb6f7610892ce8da
Roman <roman.romanov1998@gmail.com>
2016-10-18 19:17:35 +0100

    update_1_sqe.py
    I added a function "import math" to make a program more readable.

--- Solution of Quadratic Equation.py
+++ Solution of Quadratic Equation.py
@@ -1,19 +1,20 @@
 print (" This program can solve quadratic equations like a(x^2) + bx + c = 0 ")
 print()
+import math
 # Here user enter the variable value of a, b, c
 a = float(input(" Enter a "))
 b = float(input(" Enter b "))
 c = float(input(" Enter c "))
 print()
-D = b * b - 4 * a * c 
+D = math.pow(b,2) - 4 * a * c 
 float(D)
 if D > 0:
     print (" Discriminant is ", D)
-    print (" Square root of Discriminant is", D ** 0.5)
+    print (" Square root of Discriminant is", math.sqrt(D))
     print()
-    x1 = (-b + D ** 0.5) / (2 * a)
+    x1 = (-b + math.sqrt(D)) / (2 * a)
     print (" x1 is", x1)
-    x2 = (-b - D ** 0.5) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
     print (" x2 is", x2)
 elif D == 0:
     print (" Discriminant is ", D)
