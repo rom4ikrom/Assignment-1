274e2c04c3ed4d353f66346e5813c4561be1fda9
Roman <roman.romanov1998@gmail.com>
2016-10-20 18:50:35 +0100

    update_2_sqe
    Change print() to "\n". To make a code more simpler.

--- Solution of Quadratic Equation.py
+++ Solution of Quadratic Equation.py
@@ -1,29 +1,25 @@
-print (" This program can solve quadratic equations like a(x^2) + bx + c = 0 ")
-print()
+print (" This program can solve quadratic equations like a(x^2) + bx + c = 0 \n")
 import math
 # Here user enter the variable value of a, b, c
 a = float(input(" Enter a "))
 b = float(input(" Enter b "))
 c = float(input(" Enter c "))
-print()
 D = math.pow(b,2) - 4 * a * c 
 float(D)
 if D > 0:
-    print (" Discriminant is ", D)
-    print (" Square root of Discriminant is", math.sqrt(D))
-    print()
+    print (" \n Discriminant is ", D)
+    print (" Square root of Discriminant is \n", math.sqrt(D))
     x1 = (-b + math.sqrt(D)) / (2 * a)
     print (" x1 is", x1)
     x2 = (-b - math.sqrt(D)) / (2 * a)
     print (" x2 is", x2)
 elif D == 0:
-    print (" Discriminant is ", D)
+    print (" \n Discriminant is \n", D)
     print ()
     x = (-b) / (2 * a)
     print (" x is ", x)
 elif D < 0:
-    print (" Discriminant is below 0 ")
+    print (" \n Discriminant is below 0 ")
     print (" The equation has no roots ")
-print ()
-print (" Thank you for using this program. ")
+print (" \n Thank you for using this program. ")
 input()
