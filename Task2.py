1dc39e7696e56a93be5fb57f6416544e08720bdf
Roman <roman.romanov1998@gmail.com>
2016-10-21 17:46:57 +0100

    update_3_Task2.py
    I added the ability of calculation a length to circle and add strings escapes to make an output of program more readable.

--- Task2.py
+++ Task2.py
@@ -1,5 +1,7 @@
-#This is a simple program that allows user calculate an area of a circle.
+#This is a simple program that allows user calculate an area and a length of a circle.
 import math
 radius = float(input("Radius:"))
 area = math.pi * math.pow(radius, 2)
-print("Area is: ", area)
+length = 2 * math.pi * radius
+print("\nArea is: \t", area)
+print("\nLength is: \t", length)
